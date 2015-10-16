16f221157a60caccd0fff6544bef8e46fbf28a3b
Isai <blaxk_92@hotmail.com>
2015-10-16 13:41:12 -0500

    Pestaña de contacto Funcionando

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+  $destino="robles92.fce@gmail.com";
+  $asunto="comentario";
+  $nombre=$_POST['nombre'];
+  $comentario="
+    Nombre:$_POST[nombre]
+    Email:$_POST[email]
+	Comentario:$_POST[com]
+  ";
+  
+  $headers = 'From: '.$destino."\r\n".
+    'Reply-To:'.$destino."\r\n".
+    'X-Mailer: PHP/'.phpversion();
+	
+	mail($destino,$asunto,$comentario,$headers);
+	echo "<a href=\"javascript:history.go(-1)\">Mensaje Enviado. Regresar</a>";
+?>
